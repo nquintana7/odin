eb5cb917343a5a2ff1af8dde3924a946df940c97
daktei <daktei7@gmail.com>
2020-01-11 22:47:20 +0100

    Etch-A-Sketch Project added

---
+++ etch-a-sketch/README.md
@@ -0,0 +1,2 @@
+And yet another Project from The Odin Project (https://www.theodinproject.com/)
+Goal this time was create, at first, a 16x16 grid in which each cell changed color to black once the mouse was over it. Then it was extended with a button for changing the grid size and another one for changing the color to a random one instead of black.
---
+++ etch-a-sketch/scripts.js
@@ -0,0 +1,39 @@
+let container = document.getElementsByClassName("container")[0];
+let width = document.getElementsByClassName("container")[0].clientWidth;
+let height = document.getElementsByClassName("container")[0].clientHeight;
+let mode = "black";
+let size = 16;
+createGrid(size, mode);
+document.getElementById("button1").addEventListener("click", function remoCrea(){
+    var divs = document.querySelectorAll("div1");
+    for (i = 0; i < divs.length; ++i) {
+        divs[i].parentNode.removeChild(divs[i]);
+    }
+    let size = prompt("How many squares per size this time?");
+    mode = "black";
+    createGrid(size, mode);
+})
+document.getElementById("button2").addEventListener("click", function () {
+    mode = "rainbow";
+    var divs = document.querySelectorAll("div1");
+    for (i = 0; i < divs.length; ++i) {
+        divs[i].parentNode.removeChild(divs[i]);
+    }
+    let size = prompt("How many squares per size this time?");
+    createGrid(size, mode);
+})
+function createGrid(size, mode) {
+for (i = 0; i < size*size; i++) {
+    let color = "black";
+    if (mode == "rainbow") {
+        color = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+    }
+    let div = document.createElement("div1");
+    div.style.cssText = "float: left; position: relative; width: " + width/size + "px; height: " + height/size + "px;";
+    container.appendChild(div);
+    div.addEventListener("mouseover", function toBlack(){
+        div.style.backgroundColor = color;
+        div.removeEventListener("mouseover", toBlack, false);
+    },false)
+}
+}
